c51f4daeef0a819f62e70c9676f1c5128e67c6e9
Rhodri Pugh <rod@pu-gh.com>
2014-01-30 14:13:46 +0000

    added shortcuts for iphone/ipad

--- aliases.sh
+++ aliases.sh
@@ -6,6 +6,8 @@ alias c="clear"
 alias t="trt "
 alias mou="open /Applications/Mou.app"
 alias iossim="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
+alias iphone="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app/Contents/MacOS/iPhone\ Simulator -SimulateDevice 'iPhone'"
+alias ipad="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app/Contents/MacOS/iPhone\ Simulator -SimulateDevice 'iPad'"
 
 # don't bug me
 alias ack='nocorrect ack '
