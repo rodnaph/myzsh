69a5f5cb254aafc5c671c6d8d16b7df7f0f3e0e5
Rhodri Pugh <rod@pu-gh.com>
2016-06-13 15:33:28 +0100

    add helper for copying large directory

--- functions.sh
+++ functions.sh
@@ -1,3 +1,13 @@
 
 function gfm { git merge $1 --ff-only && git branch -D $1; }
 
+# copy the contents of a directory to another
+# usage: cpd ~/source/folder ~/dest/folder
+function cpd() {
+    s=$1;
+    d=$2;
+    for f in $s/*; do
+        cp $f $d;
+    done
+}
+
