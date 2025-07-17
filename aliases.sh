8921b76c47093ec65e009d7faf307648d43f7d0b
Rhodri Pugh <rod@owsy.com>
2025-07-17 13:56:01 +0100

    jaskdjaslkdjasdjalsd

--- aliases.sh
+++ aliases.sh
@@ -15,14 +15,16 @@ alias gcn="git commit --no-verify"
 alias am="git commit --amend"
 alias gc="git commit"
 alias gb="git branch --sort=-committerdate"
+alias gs="git status"
+alias gco="git checkout"
 
 # nvim
 alias vi="nvim"
 
 # git workflow
 alias pause="git add --all; git commit --no-verify -m 'WIP'"
-alias pu="docker compose exec bindhq-fpm rm -rf var/cache/test/twig; docker compose exec bindhq-fpm php -d memory_limit=2G vendor/bin/phpunit --order-by default"
-alias puu="docker compose exec bindhq-fpm rm -rf var/cache/test/twig; docker compose exec bindhq-fpm php -d memory_limit=2G vendor/bin/phpunit --order-by default tests/unit"
+alias pu="rm -rf var/cache/test/twig; php -d memory_limit=2G vendor/bin/phpunit --order-by default"
+alias puu="SYMFONY_DEPRECATIONS_HELPER='' php -d memory_limit=2G vendor/bin/phpunit --order-by default tests/unit"
 alias mps="make phpstan"
 
 # github
@@ -33,9 +35,9 @@ alias pr='pb && open "https://github.com/owsy/$(basename `git rev-parse --show-t
 alias uuidgen='uuidgen | tr "[:upper:]" "[:lower:]"'
 alias ip="curl http://wtfismyip.com/text"
 alias ll='ls -l'
-alias cs="vendor/bin/php-cs-fixer fix"
+alias cs="vendor/bin/mago fmt"
 alias m="make"
 
 # bindhq
-alias cc="rm -rf var/cache && docker compose exec bindhq-fpm rm -rf var/cache"
-alias bc="docker compose exec bindhq-fpm bin/console"
+alias cc="rm -rf var/cache && rm -rf var/cache"
+alias bc="bin/console"
